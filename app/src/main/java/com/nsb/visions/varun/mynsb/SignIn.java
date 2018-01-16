a5e4fa5355a42cffc6361f2b19448291001cfdbf
Varun Sethu <varun.sethu1@education.nsw.gov.au>
2018-01-16 19:46:12 +1100

    Pushing better code

--- app/src/main/java/com/nsb/visions/varun/mynsb/SignIn.java
+++ app/src/main/java/com/nsb/visions/varun/mynsb/SignIn.java
@@ -8,9 +8,9 @@ import android.content.res.AssetManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -26,139 +26,251 @@ import android.widget.Toast;
 import com.nsb.visions.varun.mynsb.Auth.Auth;
 import com.nsb.visions.varun.mynsb.User.User;
 
+import java.util.HashMap;
 import java.util.Locale;
 
 
 public class SignIn extends AppCompatActivity {
 
     // UI updater handler
-    private Handler UIHandler = new Handler();
+    private final Handler uiHandler = new Handler();
+    // Authenticator for authentication
+    private final Auth authenticator = new Auth(getApplicationContext());
+    // Shared preferences
+    private final SharedPreferences sharePref = getSharedPreferences("MyNSB", Context.MODE_PRIVATE);
+    // Shared preferences editor
+    private final SharedPreferences.Editor editor = sharePref.edit();
+    // Asset manager for loading various assets
+    private final AssetManager am = getApplicationContext().getAssets();
+    // Raleway font used regularly throughout page
+    private final Typeface raleway = Typeface.createFromAsset(am,
+        String.format(Locale.US, "fonts/%s", "raleway_regular.ttf"));
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        // Attain shared preferences
-        final SharedPreferences SharePref = getSharedPreferences("MyNSB", Context.MODE_PRIVATE);
-        final SharedPreferences.Editor Editor = SharePref.edit();
-
-        if (SharePref.getBoolean("firstrun", true)) {
-            Editor.putBoolean("logged-in", false);
-            Editor.putBoolean("firstrun", false);
-            Editor.apply();
-            // Show the tutorial
-        } else if (SharePref.getBoolean("logged-in", false)) {
-            Intent redirect = new Intent(SignIn.this, Home.class);
-            startActivity(redirect);
-        }
-
-
+        // Determine what to do with the incoming user through the router user function
+        routeUser(sharePref, editor);
         // Load in the layout after shared pref has been checked
         setContentView(R.layout.activity_sign_in);
 
 
-        // Loader container used in the rest of the code
+
+        // Attain all the required ui components
         final RelativeLayout loaderView = (RelativeLayout) findViewById(R.id.loaderContainer);
         final RelativeLayout mainContent = (RelativeLayout) findViewById(R.id.activity_sign_in);
-
-        // Change all the fonts
-        AssetManager Am = getApplicationContext().getAssets();
-        final Typeface Raleway = Typeface.createFromAsset(Am,
-                String.format(Locale.US, "fonts/%s", "raleway_regular.ttf"));
-
-        ((TextView) findViewById(R.id.signInText)).setTypeface(Raleway);
-
-
-
-
-        // This is the login button
+        // Title text displays: "Sign In"
+        final TextView titleText = ((TextView) findViewById(R.id.signInText));
         Button signInButton = (Button) findViewById(R.id.signInButton);
+        // Initialize the UI
+        initUI(titleText);
+
+
 
 
         // Set an onclick listener for the submit button
         signInButton.setOnClickListener(v -> {
-
-
             // Get the values from the editext fields
-            final String StudentID = ((EditText) findViewById(R.id.studentID)).getText().toString();
-            final String Password = ((EditText) findViewById(R.id.studentPassword)).getText().toString();
+            HashMap<String, String> enteredDetails = getEnteredDetails();
 
-            // Determine if they are empty
-            if (StudentID.isEmpty() || Password.isEmpty()) {
-                Toast.makeText(SignIn.this, "Student ID or Password is empty", Toast.LENGTH_LONG).show();
+            // Do a quick null check
+            if (enteredDetails == null) {
                 return;
             }
 
-            // Split the main tasks into two threads
-
-            // One for UI updates.....
-            // Start the loader animation
-            UIHandler.post(() -> {
-                mainContent.setClickable(false);
-
-                // Set the visibility
-                loaderView.setVisibility(View.VISIBLE);
-                Animation fadeIn = new AlphaAnimation(0, 1);
-                fadeIn.setInterpolator(new AccelerateInterpolator());
-                fadeIn.setDuration(200);
-
-                // Set the status bar colour
-                Window window = getWindow();
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-                window.setStatusBarColor(Color.BLACK);
-                // Set it and start
-                loaderView.startAnimation(fadeIn);
-            });
-
-
-
+            // Open the loading animation
+            // Set the background to not be clickable
+            mainContent.setClickable(false);
+            // Animate the loader into view
+            animateLoader(loaderView, true);
 
             // Thread for authentication...
             Thread auth = new Thread(() -> {
-                // Sleep the thread for that special effect ;)
-                try {
-                    Thread.sleep(300);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                HashMap<String, Object> authDetails = performAuth(enteredDetails);
 
-                // Parse the data into the auth function
-                Auth authenticator = new Auth(getApplicationContext());
-                try {
-                    User user = authenticator.auth(StudentID, Password);
-                    // Serialize the user data
-                    Editor.putString("user-data", user.toString());
-                    Editor.putBoolean("logged-in", true);
-                    Editor.apply();
-
-                    // Redirect the user
+                if ((Boolean) authDetails.get("success")) { // One line heros :p, pre much it just gets the valid flag from the validUser function {Success}
+                    // Update the shared preferences
+                    updatePrefDetails(editor, (User) authDetails.get("user"));
+                    // Redirect the user to the home page
                     Intent moveUser = new Intent(SignIn.this, Home.class);
                     startActivity(moveUser);
                     finish();
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    // Set the successful login flag to false
-                    Log.d("Error", e.toString());
-
-                    // Refresh the activity...
-                    // Close the Relative layout....
-                    UIHandler.post(() -> {
-                        // Fade out the loader view
-                        Animation fadeOut = new AlphaAnimation(1, 0);
-                        fadeOut.setDuration(150);
-                        fadeOut.setStartOffset(150);
-                        fadeOut.setInterpolator(new AccelerateInterpolator());
-
-                        loaderView.startAnimation(fadeOut);
-                        loaderView.setVisibility(View.GONE);
-
-                        Toast.makeText(SignIn.this, "Details are invalid", Toast.LENGTH_LONG).show();
-                    });
+                } else { // Failure
+                    // Fade out the loader view
+                    animateLoader(loaderView, false);
+                    // Set the background to be clickable
+                    mainContent.setClickable(true);
+                    // Alert the user that the authentication attempt was unsuccessful
+                    Toast.makeText(SignIn.this, "Details are invalid", Toast.LENGTH_LONG).show();
                 }
+
             });
             auth.start();
+
+        });
+
+        // End of main function ======================================
+    }
+
+
+
+
+
+
+    // Beginning of utility functions =======================================
+
+
+    /* validUser returns a hashmap containing the user details and a flag which tell us if the auth was a success
+        @params;
+            HashMap<String, String> enteredDetails
+     */
+    private HashMap<String, Object> performAuth(HashMap<String, String> enteredDetails) {
+        // Init the hashmap to return
+        HashMap<String, Object> toReturn = new HashMap<>();
+        try {
+            User user = authenticator.auth(enteredDetails.get("studentID"), enteredDetails.get("password"));
+            // Update the shared preferences with the details we have been given
+            toReturn.put("success", true);
+            toReturn.put("user", user);
+        } catch (Exception _) {
+            toReturn.put("success", false);
+        }
+
+        return toReturn;
+    }
+
+
+
+
+
+    /* getEnteredDetails returns a map with the currently entered username and password returns null if the details are empty
+        @params;
+            nil
+
+     */
+    @Nullable
+    private HashMap<String, String> getEnteredDetails() {
+        // Attain the details
+        final String studentID = ((EditText) findViewById(R.id.studentID)).getText().toString();
+        final String password = ((EditText) findViewById(R.id.studentPassword)).getText().toString();
+
+        if (studentID.isEmpty() || password.isEmpty()) {
+            // Post onto the main UI thread
+            uiHandler.post(() -> {
+                Toast.makeText(SignIn.this, "Student ID or Password is empty", Toast.LENGTH_LONG).show();
+            });
+            return null;
+        }
+
+        // Push the user's details into the hashmap
+        HashMap<String, String> userMap = new HashMap<>();
+        userMap.put("studentID", studentID);
+        userMap.put("password", password);
+
+        // Return the final map
+        return userMap;
+    }
+
+
+
+
+
+
+    /* initUI initialises the ui by setting the fonts and anything else u might want to happen
+        @params;
+            TextView signInText
+     */
+    private void initUI(TextView signInText) {
+        signInText.setTypeface(raleway);
+    }
+
+
+
+
+
+    /* routeUser routes the current user based on the stored shared preferences, if this is the first one a tutorial is shown if they are logged in they are taken to the home screen
+        @params;
+            SharedPreferences preferences
+            SharedPreferences.Editor editor
+        ! WARNING
+            Incomplete method, please see TODO for more details
+     */
+    private void routeUser(SharedPreferences preferences, SharedPreferences.Editor editor) {
+        // Determine if this is the first time they have run the app if so then take them to the tutorial
+        if (preferences.getBoolean("firstrun", true)) {
+            // Set the logged-in flag to false
+            editor.putBoolean("logged-in", false);
+            // Set the firstrun flag to false
+            editor.putBoolean("firstrun", false);
+            // Apply changes
+            editor.apply();
+            // Show the tutorial
+            // TODO: Once the tutorial is completed implement this redirect
+
+        // Determine if the user is logged-in through the flag
+        } else if (preferences.getBoolean("logged-in", false)) {
+            // Redirect the user to the home page
+            Intent redirect = new Intent(SignIn.this, Home.class);
+            startActivity(redirect);
+        }
+    }
+
+
+
+
+
+    /* updatePrefDetails updates the shared preferences given a specific user
+        @params;
+            SharedPreferences.Editor editor
+            User user
+     */
+    private void updatePrefDetails(SharedPreferences.Editor editor, User user) {
+        // Serialize the user data and put the string in the editor
+        editor.putString("user-data", user.toString());
+        // Set the logged in data
+        editor.putBoolean("logged-in", true);
+        // Apply our changes
+        editor.apply();
+    }
+
+
+
+
+
+
+    /* animateLoader takes the loader view and displays it to the user using a face in animation
+        @params;
+            RelativeLayout loadingScreen
+            Boolean openClose:
+                        True: open,
+                        False: close
+
+     */
+    private void animateLoader(RelativeLayout loadingScreen, Boolean openClose) {
+        // Declare animations, one for fading in the other for fading out
+        // Fade out
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setDuration(150);
+        fadeOut.setStartOffset(150);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+
+        // Fade in
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new AccelerateInterpolator());
+        fadeIn.setDuration(200);
+
+
+        // Post the fade animation to a runnable
+        uiHandler.post(() -> {
+            if (openClose) { // Open the loading screen
+                loadingScreen.setVisibility(View.VISIBLE);
+                loadingScreen.startAnimation(fadeIn);
+            } else { // Close the loading screen
+                loadingScreen.startAnimation(fadeOut);
+                loadingScreen.setVisibility(View.GONE);
+            }
         });
     }
 }
